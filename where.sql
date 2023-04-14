aeda1aab456e94c06e5a5f4a61b1a1cee65652f1
Pradeep Kumar (MERNSTACK DEVELOPER) <109032778+pradeep-cktd@users.noreply.github.com>
2023-04-14 19:59:06 +0530

    Add files via upload

---
+++ where.sql
@@ -0,0 +1,18 @@
+-- SELECT * FROM employee.emp;
+-- select * from emp;
+-- select * from emp where salary=10000;
+-- select empid,name,salary from emp where salary = 10000;
+-- select * from emp where salary > 10000; 
+-- select * from emp where salary >= 10000; 
+-- select * from emp where salary < 50000; 
+-- select * from emp where salary <= 90000; 
+-- select * from emp where salary <> 50000;
+-- select * from emp where salary != 50000;
+-- select * from emp where salary between 10000 AND 50000;
+-- select * from emp where salary between 2000 AND 5000;
+-- select * from emp;
+-- select * from emp where name like "d%";
+-- select * from emp where email like "%@hero.com";
+select * from emp where name not like "d%";
+
+
